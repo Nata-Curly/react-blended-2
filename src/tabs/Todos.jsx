3b6abeaf360738bdcab9c4fb513fc5968580727a
mariashubna <mariashubna@gmail.com>
2024-11-30 15:53:38 +0100

    /

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,9 +1,15 @@
 import { Form, Text, TodoList } from 'components';
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const Todos = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    () => JSON.parse(localStorage.getItem('todos')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  });
 
   const onSubmit = text => {
     setTodos([
@@ -22,8 +28,11 @@ export const Todos = () => {
   return (
     <>
       <Form onSubmit={onSubmit} />
-      <Text textAlign="center">There are no any todos ...</Text>
-      <TodoList todos={todos} onDelete={handleDelete} />
+      {todos.length ? (
+        <TodoList todos={todos} onDelete={handleDelete} />
+      ) : (
+        <Text textAlign="center">There are no any todos ...</Text>
+      )}
     </>
   );
 };
